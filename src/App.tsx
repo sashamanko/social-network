cb578d3a2c9f1edc11851347af6c962c27ff3a02
sashamanko <mankooleksandr748@gmail.com>
2022-06-17 15:25:24 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useRef, useState } from "react";
+
+import { signup, login, logout, useAuth } from "./firebase/firebase";
+
+const App = () => {
+  const [ loading, setLoading ] = useState(false);
+  const currentUser: any = useAuth();
+
+  const emailRef: any = useRef();
+  const passwordRef: any = useRef();
+
+  async function handleSignup() {
+    setLoading(true);
+    try {
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      alert("Error!");
+    }
+    setLoading(false);
+  }
+
+  async function handleLogin() {
+    setLoading(true);
+    try {
+      await login(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      alert("Error!");
+    }
+    setLoading(false);
+  }
+
+  async function handleLogout() {
+    setLoading(true);
+    try {
+      await logout();
+    } catch {
+      alert("Error!");
+    }
+    setLoading(false);
+  }
+
+  return (
+    <div id="main">
+      
+      <div>Currently logged in as: {  currentUser?.email } </div>
+
+      <div id="fields">
+        <input ref={emailRef} placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+      </div>
+
+      <button disabled={ loading || currentUser } onClick={handleSignup}>Sign Up</button>
+      <button disabled={ loading || currentUser } onClick={handleLogin}>Log In</button>
+      <button disabled={ loading || !currentUser } onClick={handleLogout}>Log Out</button>
+
+    </div>
+  );
+};
+
+export default App;
