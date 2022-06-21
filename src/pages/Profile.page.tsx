88281440dd0025e364f04e8e840ca0b8d8cd19d1
Vlad Rozanov <vvladrozanov@gmail.com>
2022-06-21 18:11:20 +0300

    init

--- src/pages/Profile.page.tsx
+++ src/pages/Profile.page.tsx
@@ -1,10 +1,21 @@
 // import './ProfilePage.css';
 
+import { useAuth } from "../hooks";
+
 const ProfilePage = () => {
+
+  const {email} = useAuth();
+
   return (
-    <>
-      
-    </>
+    <div>
+      <div className="profileInfo flex mt-1 item-block">
+        <div style={{background: 'red', width: '200px', height: '200px'}} className="profileIMG rounded-fill mr-1"></div>
+        <div className="flex flex-col">
+          <h2>Username</h2>
+          <p>{email}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
